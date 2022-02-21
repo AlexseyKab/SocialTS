b5cec242a4234445e94546708466be018f94a198
AlexseyKab <help_me101@mail.ru>
2022-02-21 14:28:49 +0300

    commit

--- src/components/Users/UsersClass.tsx
+++ src/components/Users/UsersClass.tsx
@@ -13,9 +13,7 @@ type UsersType = {
 }
 
 class UsersClass extends React.Component<UsersType> {
-    constructor(props: UsersType) {
-        super(props);
-
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             this.props.setUsers(response.data.items)
         })

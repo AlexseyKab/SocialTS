9d3012ff5fe759376b38a1ece7e37dcd8fc93ec3
AlexseyKab <help_me101@mail.ru>
2022-02-21 13:49:36 +0300

    commit

--- src/components/Users/UsersClass.tsx
+++ src/components/Users/UsersClass.tsx
@@ -13,18 +13,17 @@ type UsersType = {
 }
 
 class UsersClass extends React.Component<UsersType> {
-    getUsers = () => {
+    constructor(props: UsersType) {
+        super(props);
 
-        if (this.props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                this.props.setUsers(response.data.items)
-            })
-        }
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setUsers(response.data.items)
+        })
     }
+
 
     render() {
         return <div>
-            <button onClick={this.getUsers}>Get Users</button>
             {
                 this.props.users.map(u => <div key={u.id}>
                 <span>

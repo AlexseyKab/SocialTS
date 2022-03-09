340328f939c9f4cc8a8760bbae0d48bcdda25f25
AlexseyKab <help_me101@mail.ru>
2022-03-09 12:52:18 +0300

    commit

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -3,7 +3,21 @@ import React from "react";
 const Login = () => {
     return (
         <div>
-            Login
+          <h1>Login</h1>
+            <form>
+                <div>
+                    <input placeholder={'Login'} />
+                </div>
+                <div>
+                    <input placeholder={'Password'} />
+                </div>
+                <div>
+                    <input type={'checkbox'}/> remember me
+                </div>
+                <div>
+                    <button>Login</button>
+                </div>
+            </form>
         </div>
     )
 }
